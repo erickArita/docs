efaabfa5b1e5563e2ffa3df47492ed27122710ea
erickArita <erick.emao@gmail.com>
2020-10-11 19:37:16 -0600

    boton link

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/docs/precache-manifest.df235c15bf05b6edcfc1aa39d7ba8b5a.js"
+  "/docs/precache-manifest.a9bf571d2b40091fbd5cb1cc35d53cd0.js"
 );
 
 self.addEventListener('message', (event) => {
